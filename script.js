461469e3e1619c249373a8e86b3905678db2b240
jonnivar83 <58147335+jonnivar83@users.noreply.github.com>
2020-03-01 15:14:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// question 1
+var petArray = ["cat", "cow", "dog"];
+// question 2
+var petArray = ["cat", "cow", "dog"];
+console.log("cow");
+// question 3
+var petArray = ["cat", "cow", "dog"];
+petArray.push("sheep");
+console.log(petArray);
+// question 4
+console.log(petArray.length);
+// question 5
+var catObject = {
+    name: "NonStop",
+    color: "black", 
+    age: 9
+};
+// question 6
+console.log(catObject.age)
+// question 7
+var catArray = [
+    {name: "NonStop",
+color: "black", 
+age: 9}
+]
+// question 8
+for(var i = 0; i < posts.length; i++) {
+console.log(posts[i].name);
+console.log(posts[i].color);
+console.log(posts[i].age);
+}
+// question 9
+function logToConsole (NonStop){
+    Console.log("NonStop")
+}
+logToConsole();
+
+// question 10
+function logToConsole (NonStop){
+    Console.log("NonStop")
+}
+logToConsole("donkey");
